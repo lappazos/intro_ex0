cb5b806c116d933c0bf33a604bf0a628d8e9d7f1
Lior Paz <44434045+lappazos@users.noreply.github.com>
2018-11-07 13:22:38 +0200

    Add files via upload

---
+++ ex0.py
@@ -0,0 +1,8 @@
+##################################################################
+# FILE : ex0.py
+# WRITER : Lior Paz, lioraryepaz, 206240996
+# EXERCISE : intro2cs ex0 2017-2018
+# DESCRIPTION : A simple program that prints "Hello World!" to
+# the standard output (screen).
+##################################################################
+print("Hello World!")
